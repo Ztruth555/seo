732d9cf49ab19c4f40f5f1836383126dfc51b0d2
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-04-28 19:36:41 +0500

    fix: section hero bugs

--- send.php
+++ send.php
@@ -29,7 +29,7 @@ try {
   $mail->Host       = "smtp.gmail.com";                         // Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
   $mail->Username   = 'vasya555vas@gmail.com';               // SMTP username
-  $mail->Password   = '7788olala6';                         // SMTP password
+  $mail->Password   = '7788olala6';                     // SMTP password
   $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
   $mail->Port       = 465;                                    // TCP port to connect to
 
