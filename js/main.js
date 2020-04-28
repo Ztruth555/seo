5b7a5dc12034e28a0f7fec6ac4acfa9069b1377c
Igor Laptev <igorlaptev@MacBook-Pro-Igor.local>
2020-04-28 15:11:37 +0500

    feat: added anchor-links and made some fixes

--- js/main.js
+++ js/main.js
@@ -264,5 +264,31 @@ $(document).ready(function () {
   //   },
   // });
 
+
+  // Плавная прокрутка при нажатие на якорную ссылку
+  jQuery(function($){
+    $('a[href*="#"]').on('click.smoothscroll', function (e) {
+    var hash = this.hash,
+        _hash = hash.replace(/#/, ''),
+        theHref = $(this).attr('href').replace(/#.*/, '');
+
+
+    if (theHref && location.href.replace(/#.*/, '') != theHref) return;
+
+      var $target = _hash === '' ? $('body') : $(hash + ', a[id="' + _hash + '"]').first();
+
+      if (!$target.length) return;
+
+      e.preventDefault();
+
+      $('html, body').stop().animate({
+        scrollTop: $target.offset().top - 0
+      }, 600, 'swing', function () {  // 400 это время прокрутки в миллисекундах
+        window.location.hash = hash;
+      });
+
+    });
+  });
+
   
 })
